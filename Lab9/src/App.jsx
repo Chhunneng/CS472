d488ac3af6d22915ecf74fdd06bc6082d1e1bc1d
Chrea Chanchhunneng <chreachanchhunneng@gmail.com>
2025-05-07 14:42:55 -0500

    add lab9 dist

--- Lab9/src/App.jsx
+++ Lab9/src/App.jsx
@@ -2,7 +2,10 @@ import "./App.css";
 import ProductsList from "./Components/ProductsList";
 
 function App() {
-    return <ProductsList />;
+    return <>
+    <h3><a href="https://github.com/Chhunneng/CS472/tree/main/Lab9">Here is code</a></h3>
+    <ProductsList />
+    </>;
 }
 
 export default App;
